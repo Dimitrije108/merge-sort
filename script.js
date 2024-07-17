c4aefd570eca1d88439d2a5106e3431c166531c3
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-07-17 13:41:34 +0200

    fix: potential issue with merge while loop and ensure an integer is returned when dividing array

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 function merge(arr1, arr2) {
   let sorted = [];
 
-  while (arr1[0] >= 0 && arr2[0] >= 0) {
+  while (arr1.length > 0 && arr2.length > 0) {
     if (arr1[0] < arr2[0]) {
       sorted.push(arr1[0]);
-      arr1.splice(0, 1);
+      arr1.shift();
     } else {
       sorted.push(arr2[0]);
-      arr2.splice(0, 1);
+      arr2.shift();
     }
   }
 
@@ -25,10 +25,10 @@ function merge(arr1, arr2) {
 }
 
 function mergeSort(arr) {
-  if (arr.length === 1) {
+  if (arr.length <= 1) {
     return arr;
   } else {
-    let mid = arr.length / 2;
+    let mid = Math.floor(arr.length / 2);
     let left = mergeSort(arr.slice(0, mid));
     let right = mergeSort(arr.slice(mid, arr.length));
     return merge(left, right);
